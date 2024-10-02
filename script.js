e913ab032de0b58181cf63a02ab8288fc827b6d5
Victoria <shevvictoryia@gmail.com>
2024-10-02 13:33:34 +0300

    feat: add pipe to game

---
+++ script.js
@@ -0,0 +1,8 @@
+const body = document.querySelector("body");
+
+const pipe = document.createElement("img");
+pipe.className = "pipe";
+pipe.setAttribute("src", "./assets/images/pipe.png");
+pipe.setAttribute("alt", "pipe");
+
+body.appendChild(pipe);
